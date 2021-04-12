f40dc741d174a3c99f97c33040137ca7ebaeebda
Gary <hokaixiong@hotmail.com>
2021-04-13 00:09:15 +0800

    include minutes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 window.onload = function(){
     var tens = 00;
     var seconds = 00;
-    var appendTens = document.getElementById("tens");
+    var minutes = 00;
+    var appendMinutes = document.getElementById("minutes");
     var appendSeconds = document.getElementById("seconds");
+    var appendTens = document.getElementById("tens");
     var buttonStart = document.getElementById("button-start");
     var buttonStop = document.getElementById("button-stop");
     var buttonReset = document.getElementById("button-reset");
@@ -22,35 +24,55 @@ buttonStop.onclick = function(){
 
 buttonReset.onclick = function(){
     clearInterval(Interval);
-    tens  = "00";
+    minutes  = "00";
     seconds = "00";
-    appendTens.innerHTML = tens;
+    tens = "00";
+    appendMinutes.innerHTML = minutes;
     appendSeconds.innerHTML = seconds;
+    appendTens.innerHTML = tens;
 }
 
 
 function startTimer() {
     tens++;
+    
 
-    if(tens <= 9){
+    if(tens <=9){
         appendTens.innerHTML = "0" + tens;
     }
 
-    if(tens > 9){
+    if(tens >9){
         appendTens.innerHTML = tens;
     }
-
+    
     if(tens > 99){
+
         console.log("seconds");
         seconds++;
         appendSeconds.innerHTML = "0" + seconds;
         tens = 0;
         appendTens.innerHTML = "0" + 0;
+
+    if(seconds <= 9){
+        appendSeconds.innerHTML = "0" + seconds;
     }
 
     if(seconds > 9){
         appendSeconds.innerHTML = seconds;
     }
+
+    if(seconds > 99){
+        console.log("minutes");
+        minutes++;
+        appendMinutes.innerHTML = "0" + minutes;
+        seconds = 0;
+        appendSeconds.innerHTML = "0" + 0;
+    }
+
+    if(minutes > 9){
+        appendMinutes.innerHTML = minutes;
+    }
+}
 }
 
 }
